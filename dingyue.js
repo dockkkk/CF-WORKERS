587dfc8cd17d241336eaff0ea9928ca406a11657
dockkkk <102992310+dockkkk@users.noreply.github.com>
2025-05-08 16:48:39 +0800

    Update dingyue.js

--- dingyue.js
+++ dingyue.js
@@ -3,7 +3,7 @@
 
 let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 
-// 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
+// 设置优选地址，不带端口号默认443，不支持非TLS订阅
 let addresses = [
 	'icook.tw:2053#优选域名',
 	'cloudflare.cfgo.cc#优选官方线路',
